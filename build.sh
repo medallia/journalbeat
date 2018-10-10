4334b3f4e476a8992f93946c2cc5ab47b1fc65aa
Diego Erdody <diego@medallia.com>
2018-10-10 12:05:06 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -ex
 go build -v
 tar -czvf journalbeat.tar.gz journalbeat
 fpm -s tar -t rpm -n journalbeat -v 5.1.2 journalbeat.tar.gz
+fpm -s tar -t deb -n journalbeat -v 5.1.2 journalbeat.tar.gz
 mv journalbeat*.rpm build/
+mv journalbeat*.deb build/
 mv journalbeat build/
 rm journalbeat.tar.gz
