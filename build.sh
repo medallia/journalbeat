19ff0cc8d8b33648b25d1a7e11dfaf9790c69f96
Diego Erdody <diego@medallia.com>
2018-10-17 14:53:30 -0700

    Fix package creation parameters

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -ex
 go build -v
 tar -czvf journalbeat.tar.gz journalbeat
-fpm -s tar -t rpm -n journalbeat -v 5.1.2 journalbeat.tar.gz
-fpm -s tar -t deb -n journalbeat -v 5.1.2 journalbeat.tar.gz
+fpm -s tar -t rpm -n journalbeat -v 5.1.2 --prefix /usr/sbin journalbeat.tar.gz
+fpm -s tar -t deb -n journalbeat -v 5.1.2 --prefix /usr/sbin journalbeat.tar.gz
 mv journalbeat*.rpm build/
 mv journalbeat*.deb build/
 mv journalbeat build/
